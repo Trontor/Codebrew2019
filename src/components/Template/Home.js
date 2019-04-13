1078bf4ba82a93ad41a253ab5f4d23af5eed021a
lsp0 <liamsaliba@hotmail.com>
2019-04-13 18:15:43 +1000

    add template for thing

---
+++ src/components/Template/Home.js
@@ -0,0 +1,15 @@
+import "./Home.css";
+import React, { Component } from "react";
+
+
+class Login extends Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    );
+  }
+}
+
+export default Login;
