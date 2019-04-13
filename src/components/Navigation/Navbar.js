cafe444ac5ab03e6076858bbf2c6c68044f72ffc
lsp0 <liamsaliba@hotmail.com>
2019-04-13 15:27:14 +1000

    Add navbar

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <>
+        this is a Navbar
+      </>
+    );
+  }
+}
+
+export default Navbar;
