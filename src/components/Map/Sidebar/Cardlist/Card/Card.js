eff668a2d4bc8ba0f979a2f7f79c7aeb3e3ccfc0
Rohyl <rohyljoshi@hotmail.com>
2019-04-13 18:10:43 +1000

    Kinda fixed cards

--- src/components/Map/Sidebar/Cardlist/Card/Card.js
+++ src/components/Map/Sidebar/Cardlist/Card/Card.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./Card.css";
 const Card = props => {
   return (
-    <div className="card" style={{ width: "100%" }}>
-      <div className="card-body row ">
-        <div className="col col-sm-2">
-          <img className="busker-icon img-responsive" src={props.src} />
+    <div className="card m-2" style={{ width: "100%" }}>
+      <div className="card-body row px-3 ">
+        <div className="col-lg-3 w-100 h-100">
+          <img src={props.src} className="img-fluid" />
         </div>
-        <div className="col-sm-auto img-thumbnail">
-          <p className="card-title">{props.name}</p>
-          <p className="card-text">{props.description}</p>
-          <span className="distance">2km</span>
+        <div className="col-lg-9 align-middle">
+          <span className="m-auto">{props.name}</span>
         </div>
       </div>
     </div>
