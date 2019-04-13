7ba3e060bdf99619a32a00a84ef92591c3cd2dcf
lsp0 <liamsaliba@hotmail.com>
2019-04-13 14:35:04 +1000

    Center login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,10 +7,9 @@ class Login extends Component {
     return (
       <>
         <div className="container">
-          <div className="row">
+          <div className="row h-100 row align-items-center">
             <div
-              className="col-sm-9 col-md-7 col-lg-5 mx-auto"
-              style={{ marginTop: 0 }}
+              className="col-sm-9 col-md-7 col-lg-5 mx-auto my-auto"
             >
               <div className="card card-signin my-3">
                 <div
