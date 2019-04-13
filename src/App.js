0510e3cfdefd3e589806916c71c2a8665db4f0b6
Rohyl <rohyljoshi@hotmail.com>
2019-04-14 01:20:28 +1000

    Changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
 import Navbar from "./components/Navigation/Navbar";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Map from "./components/Map/Map";
 import Homepage from "./components/Homepage/Homepage";
 import Profile from "./components/Profile/Profile";
@@ -11,13 +11,13 @@ class App extends Component {
   render() {
     return (
       <>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
           {/* <Navbar /> */}
           <Route exact path="/" component={Homepage} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/local" component={Map} />
           <Route exact path="/profile/:id" component={Profile} />
-        </BrowserRouter>
+        </HashRouter>
       </>
     );
   }
