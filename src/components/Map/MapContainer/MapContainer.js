dc3c8b3f7036bcf21b6d6783ca70938ed2bb78ac
lsp0 <liamsaliba@hotmail.com>
2019-04-14 11:03:58 +1000

    Revert "Revert "current location""
    This reverts commit 77b3884633a7417992eeb0ba3d26100a4e565c5c.

--- src/components/Map/MapContainer/MapContainer.js
+++ src/components/Map/MapContainer/MapContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import "./MapContainer.css";
 
+
 const AnyReactComponent = ({ imgsrc }) => (
   <div>
     <div className="roundedmarker">
@@ -16,11 +17,48 @@ class SimpleMap extends Component {
       lat: -37.8138,
       lng: 144.9646
     },
-    zoom: 16
+    zoom: 16,
   };
+  state = { userLocation: { lat: 32, lng: 32 }, loading: true };
+
+  componentDidMount(props) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        console.log(position.coords);
+        this.setState({
+          userLocation: { lat: latitude, lng: longitude },
+          loading: false
+        });
+      },
+      () => {
+        this.setState({ loading: false });
+      }
+    );
+  }
 
   render() {
-    console.log(this.props.profiles);
+    if (this.state.loading){
+      return (
+        // Important! Always set the container height explicitly
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyCFERBzLDWtOuCyokpgIN3izhT8wokMG28" }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent lat={-37.8138} lng={144.9646} text="My Marker" />
+            {this.props.profiles.filter(x=> x.live === "y").map(val => (
+              <AnyReactComponent
+                imgsrc={val.imageURL}
+                lat={val.lat}
+                lng={val.long}
+              />))}            
+          </GoogleMapReact>
+        </div>
+      );
+    }
+    console.log(this.state.userLocation)
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: "100vh", width: "100%" }}>
@@ -29,14 +67,20 @@ class SimpleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
+        <AnyReactComponent
+              imgsrc={require("../../../images/currentpin.png")}
+              lat={this.state.userLocation.lat}
+              lng={this.state.userLocation.lng}
+            />
           <AnyReactComponent lat={-37.8138} lng={144.9646} text="My Marker" />
           {this.props.profiles.filter(x=> x.live === "y").map(val => (
             <AnyReactComponent
               imgsrc={val.imageURL}
               lat={val.lat}
-              lng={val.long}
-            />
-          ))}
+              lng={val.lng}
+            />))}
+            
+          
         </GoogleMapReact>
       </div>
     );
